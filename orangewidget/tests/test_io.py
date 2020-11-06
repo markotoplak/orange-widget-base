3325b8b39e28e54c376e48e74ffd2f9312086d66
Marko Toplak <marko.toplak@gmail.com>
2020-11-06 10:49:07 +0100

    size tests

--- orangewidget/tests/test_io.py
+++ orangewidget/tests/test_io.py
@@ -83,6 +83,27 @@ class TestPdf(GuiTest):
             imgio.PdfFormat.write(fname, graph)
             with open(fname, "rb") as f:
                 self.assertTrue(f.read().startswith(b'%PDF'))
+            size_empty = os.path.getsize(fname)
+        finally:
+            os.unlink(fname)
+
+        # does a ScatterPlotItem increases file size == is it drawn
+        graph = pyqtgraph.PlotWidget()
+        graph.addItem(pyqtgraph.ScatterPlotItem(x=list(range(100)), y=list(range(100))))
+        try:
+            imgio.PdfFormat.write(fname, graph)
+            print(os.path.getsize(fname))
+            self.assertGreater(os.path.getsize(fname), size_empty + 10000)
+        finally:
+            os.unlink(fname)
+
+        # does a PlotCurveItem increases file size == is it drawn
+        graph = pyqtgraph.PlotWidget()
+        graph.addItem(pyqtgraph.PlotCurveItem(x=list(range(100)), y=list(range(100))))
+        try:
+            imgio.PdfFormat.write(fname, graph)
+            print(os.path.getsize(fname))
+            self.assertGreater(os.path.getsize(fname), size_empty + 2000)
         finally:
             os.unlink(fname)
 
